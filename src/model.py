19abd2bc4890605ad965173c463c37d433974d13
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2021-01-17 16:25:33 +0000

    replace wandb config with hparams dict

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
 """Model defined here."""
+
+from typing import Dict, Union
+
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-import wandb
 
 from .dataset import CustomDataset
 
@@ -16,49 +17,40 @@ class T5Finetuner(pl.LightningModule):
 
     Args:
         path_to_dataset (str): string path to dataset
-        config (wandb.sdk.wandb_config.Config): config dictionary containing hparams
-        model_name (str, optional): t5 model name. Defaults to "t5-small".
+        hparams (Dict): dictionary containing hyperparameters
+        model_name (str, optional): t5 model name
     """
 
     def __init__(
         self,
         path_to_dataset: str,
-        config: wandb.sdk.wandb_config.Config,
-        model_name: str = None,
+        hparams: Dict[str, Union[float, int]],
+        model_name: str,
     ):
 
         super().__init__()
 
-        if not model_name:
-            model_name = "t5-small"
-
+        self.hparams = hparams
         self.tokenizer = T5Tokenizer.from_pretrained(model_name, truncation=True)
+        self.tokenizer.add_tokens(["<speech_part>", "<def>", "<example>"])
 
         df = pd.read_csv(path_to_dataset)
-        train, validate, test = np.split(df, [int(0.8 * len(df)), int(0.9 * len(df))])
+        df_train, df_validate = np.split(df, int(0.9 * len(df)))
 
         self.model = T5ForConditionalGeneration.from_pretrained(model_name)
 
         self.training_set = CustomDataset(
-            train.reset_index(drop=True),
+            df_train.reset_index(drop=True),
             self.tokenizer,
-            config.MAX_INPUT_LEN,
-            config.MAX_OUTPUT_LEN,
+            self.hparams.MAX_INPUT_LEN,
+            self.hparams.MAX_OUTPUT_LEN,
         )
         self.val_set = CustomDataset(
-            validate.reset_index(drop=True),
+            df_validate.reset_index(drop=True),
             self.tokenizer,
-            config.MAX_INPUT_LEN,
-            config.MAX_OUTPUT_LEN,
+            self.hparams.MAX_INPUT_LEN,
+            self.hparams.MAX_OUTPUT_LEN,
         )
-        self.test_set = CustomDataset(
-            test.reset_index(drop=True),
-            self.tokenizer,
-            config.MAX_INPUT_LEN,
-            config.MAX_OUTPUT_LEN,
-        )
-
-        self.config = config
 
     def forward(
         self,
@@ -84,7 +76,11 @@ class T5Finetuner(pl.LightningModule):
         )
         y_ids = y[:, :-1].contiguous()
         lm_labels = y[:, 1:].clone()
+
+        # Replace pad token id with -100
+        # See https://github.com/huggingface/transformers/issues/6238
         lm_labels[y[:, 1:] == pad_token_id] = -100
+
         outputs = self(
             source_ids,
             attention_mask=source_mask,
@@ -106,42 +102,10 @@ class T5Finetuner(pl.LightningModule):
         generated_ids = self.model.generate(
             input_ids=source_ids,
             attention_mask=source_mask,
-            max_length=self.config.MAX_OUTPUT_LEN,
-            num_beams=2,
-            repetition_penalty=2.5,
-            length_penalty=1.0,
-            early_stopping=True,
-        )
-        preds = [
-            self.tokenizer.decode(
-                g, skip_special_tokens=True, clean_up_tokenization_spaces=True
-            )
-            for g in generated_ids
-        ]
-        target = [
-            self.tokenizer.decode(
-                t, skip_special_tokens=True, clean_up_tokenization_spaces=True
-            )
-            for t in y
-        ]
-
-        return preds, target
-
-    def test_step(self, batch, batch_idx):
-
-        source_ids, source_mask, y = (
-            batch["source_ids"],
-            batch["source_mask"],
-            batch["target_ids"],
-        )
-
-        generated_ids = self.model.generate(
-            input_ids=source_ids,
-            attention_mask=source_mask,
-            max_length=self.config.MAX_OUTPUT_LEN,
-            num_beams=2,
-            repetition_penalty=2.5,
-            length_penalty=1.0,
+            max_length=self.hparams.MAX_OUTPUT_LEN,
+            num_beams=self.hparams.NUM_BEAMS,
+            repetition_penalty=self.hparams.REPETITION_PENALTY,
+            length_penalty=self.hparams.LENGTH_PENALTY,
             early_stopping=True,
         )
         preds = [
@@ -161,17 +125,23 @@ class T5Finetuner(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(
-            params=self.model.parameters(), lr=self.config.LEARNING_RATE
+            params=self.model.parameters(), lr=self.hparams.LEARNING_RATE
         )
 
     def train_dataloader(self):
 
-        return DataLoader(self.training_set, batch_size=self.config.TRAIN_BATCH_SIZE)
+        return DataLoader(
+            self.training_set,
+            batch_size=self.hparams.TRAIN_BATCH_SIZE,
+            num_workers=8,
+            pin_memory=True,
+        )
 
     def val_dataloader(self):
 
-        return DataLoader(self.val_set, batch_size=self.config.VALID_BATCH_SIZE)
-
-    def test_dataloader(self):
-
-        return DataLoader(self.test_set, batch_size=self.config.VALID_BATCH_SIZE)
+        return DataLoader(
+            self.val_set,
+            batch_size=self.hparams.VALID_BATCH_SIZE,
+            num_workers=8,
+            pin_memory=True,
+        )
